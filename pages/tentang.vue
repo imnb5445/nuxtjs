ec459ce910448087a9164bf53edbf57c6d51b154
wayanAri <iwayanariutra13@gmail.com>
2024-08-20 11:08:43 +0800

    menambahakan page tentang

---
+++ pages/tentang.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Tentang Kami</h1>
+    <p>Ini adalah halaman tentang kami.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // Konten logika komponen
+}
+</script>
+
+<style>
+/* Gaya CSS untuk halaman tentang */
+</style>
