d051a1b5bf0d095c4106317bab9eb4c6a00a86b9
wayanAri <iwayanariutra13@gmail.com>
2024-08-20 12:36:21 +0800

    addin layout to index and tentang

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <header>
+        <div class="nav-bar">
+            <NuxtLink class="nav-item" to="/">Home</NuxtLink>
+            <NuxtLink class="nav-item" to="/tentang">About Us</NuxtLink>
+        </div>
+    </header>
+    <main>
+        <slot />
+    </main>
+    
+    <footer>
+        
+    </footer>
+</div>
+</template>
+
+<style>
+    main{
+        padding: 1rem;
+        padding-top: 5rem;
+    }
+
+    .nav-bar{
+        position: fixed;
+        margin-top: -1rem;
+        margin-left: -1rem;
+        width: 100%;
+        height: 3rem;
+        padding: 1rem;
+        background-color: black;
+        display: flex;
+    }
+
+    .nav-item{
+        margin-right: 1rem;
+        margin-left: 1rem;
+        margin-top: 0.5rem;
+        font-size: 1.5rem;
+        color: white;
+        text-decoration: none;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template> 
+
   <div>
-    <h1>Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component
-    </AppAlert>
+        <tentang />
+        <h1>Welcome to the homepage</h1>
+        <AppAlert>
+        This is an auto-imported component
+        </AppAlert>
+
   </div>
+  
+
 </template>
+
+<script >
+
+</script>
+
+<style>
+    h1{
+        color: aqua;
+    }
+</style>
