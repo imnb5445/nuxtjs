1a0f7f372468871e3ed1bc6d3374c0a90d4d6a61
wayanAri <iwayanariutra13@gmail.com>
2024-09-04 10:20:17 +0800

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,8 +11,10 @@ export default defineNuxtConfig({
     public: {
       login: process.env.LOGIN,
       test: process.env.TEST_VAR,
-      supaBase_Url: process.env.SUPABASE_URL,
-      supaBase_Key: process.env.SUPABASE_KEY,
+      supaBase_Url: "https://ehkuhspixiclvrzyeshh.supabase.co",
+      supaBase_Key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoa3Voc3BpeGljbHZyenllc2hoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2Mzc1NjQsImV4cCI6MjA0MDIxMzU2NH0.K3AXi_1QZ61ZIWkay4TM98kxHLq6rTYDJ7k2gNjvjT8",
+
+      
      
     },
    
--- pages/product_data.vue
+++ pages/product_data.vue
@@ -1,21 +1,35 @@
 <template>
     <section class="product-data-container">
         <table class="product-data-table">
-            <tr class="product-table-header-row">
+            <tr class="product-table-row">
                 <th class="product-table-header">Id</th>
                 <th class="product-table-header">Nama Produk</th>
                 <th class="product-table-header">Harga</th>
             </tr>
-            <tr class="product-table-row">
-                <th class="product-table-item">1</th>
-                <th class="product-table-item">Ayam</th>
-                <th class="product-table-item">500</th>
+            <tr class="product-table-row" v-for="product in products" :key="product.produk_id">
+                <td class="product-table-item">{{ product.produk_id }}</td>
+                <td class="product-table-item">{{ product.nama_produk }}</td>
+                <td class="product-table-item">{{ product.harga }}$</td>
             </tr>
         </table>
     </section>
 </template>
 
+<script setup>
+   const supabase = useSupabaseClient()
+const products = ref([])
 
+async function getProduct() {
+  const { data } = await supabase.from('tb-produk').select()
+  products.value = data
+
+
+}
+
+onMounted(() => {
+  getProduct()
+})
+</script>
 <style>
     .product-data-container{
         width: 100%;
@@ -25,12 +39,19 @@
         justify-content: center; */
     }
 
-    .product-data-table{
-        width: 100%;
-        border: black 5px;
+    .product-table-row:nth-child(even){
+        background-color: rgb(152, 152, 152);
     }
 
-    .product-table-header-row{
-        width: 100%;
+    .product-data-table{
+        border-collapse: collapse;
+    }
+
+    .product-table-header{
+        border-bottom: black 1px solid;
+    }
+
+    .product-table-item{
+        border-bottom: black 1px solid;
     }
 </style>
